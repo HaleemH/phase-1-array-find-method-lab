ad31f799995aa9fef77ffe60c5b3212ce386af0f
Haleem Hamid <hhamidbiz@gmail.com>
2021-12-16 10:39:40 -0800

    commit tree 4a843480efe16b38efdacaa95cae46b90e754f25

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arg) {
+    let winner = arg.find(item => item.result === "W")
+    if (winner.result != "W") {
+        return 'undefined' 
+    } else { return winner.year};
+}
+
+
